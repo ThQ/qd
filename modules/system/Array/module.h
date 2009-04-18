799bcc950dcfb206f024a099a643573dfae6850d
thomas <thomas.quemard@gmail.com>
2009-04-18 21:16:58 +0200

    modules/system/Array : Using new namespaces etc

--- modules/system/Array/module.h
+++ modules/system/Array/module.h
@@ -1,32 +1,32 @@
 #ifndef MODULES_SYSTEM_ARRAY_H
 #define MODULES_SYSTEM_ARRAY_H
 
-#include "svm/Array.h"
+#include "vm/Array.h"
 
 namespace modules { namespace system
 {
    class Array
    {
-      public: static void declare_methods(svm::Engine& engine)
+      public: static void declare_methods(t::Engine& engine)
       {
-         SVM_ASSERT_NOT_NULL(svm::array_type);
+         SVM_ASSERT_NOT_NULL(t::tARRAY);
       }
 
       public: static void finalize()
       {
-         ASSERT_NULL(svm::array_type);
+         ASSERT_NULL(t::tARRAY);
       }
 
       public: static void initialize(svm::Engine& engine)
       {
-         svm::array_type = engine.classes.declare_class("system.Array", svm::object_type);
-         SVM_ASSERT_CLASS(svm::array_type);
-         SVM_PICK(svm::array_type);
+         t::tARRAY = engine.classes.declare_class("system.Array", t::tOBJECT);
+         t::Class::assert(t::tARRAY);
+         t::Object::pick(t::tARRAY);
       }
 
       public: static void tear_down(svm::Engine& engine)
       {
-         SVM_DROP(svm::array_type);
+         t::Object::drop(t::tARRAY);
       }
    };
 } }
