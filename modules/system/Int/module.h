cea09186cff130e6d43fddc00f8879e90024fd5b
thomas <thomas.quemard@gmail.com>
2009-04-19 18:14:14 +0200

    modules/system/Int : Using new namespaces

--- modules/system/Int/module.h
+++ modules/system/Int/module.h
@@ -7,14 +7,13 @@ include(`../../module.m4')
 
 #include <stdio.h>
 
-#include "debug.h"
-#include "svm/Bool.h"
-#include "svm/Class.h"
-#include "svm/CoreFunction.h"
-#include "svm/Engine.h"
-#include "svm/Int.h"
-#include "svm/String.h"
-#include "svm/Variable.h"
+#include "t/Bool.h"
+#include "t/Class.h"
+#include "t/CoreFunction.h"
+#include "t/Int.h"
+#include "t/String.h"
+#include "t/Variable.h"
+#include "vm/Engine.h"
 
 namespace modules { namespace system {
 
@@ -22,14 +21,14 @@ namespace modules { namespace system {
    {
       public: static SVM_CLASS_METHOD(add)
       {
-         svm::Int::add_to(argv[0], argv[1]);
+         t::Int::add_to(argv[0], argv[1]);
          return argv[0];
       }
 
       public: static SVM_CLASS_METHOD(compare_to)
       {
-         Short i = svm::Int::compare_to(argv[0], argv[1]);
-         return svm::Int::build((Long)i);
+         Short i = t::Int::compare_to(argv[0], argv[1]);
+         return t::Int::build((Long)i);
       }
 
       public: static SVM_CLASS_METHOD(decrement)
@@ -44,37 +43,30 @@ namespace modules { namespace system {
 
       public: static SVM_CLASS_METHOD(equals)
       {
-         SVM_ASSERT_INT(argv[0]);
-         SVM_ASSERT_INT(argv[1]);
+         t::Int::assert(argv[0]);
+         t::Int::assert(argv[1]);
 
-         svm::Object* result;
+         t::Object* result;
 
-         if (((svm::Int*)argv[0])->value == ((svm::Int*)argv[1])->value)
-         {
-            result = (svm::Object*)svm::True;
-         }
-         else
-         {
-            result = (svm::Object*)svm::False;
-         }
-         return result;
+         bool equals = ((t::Int*)argv[0])->value == ((t::Int*)argv[1])->value;
+         return equals ? t::gTRUE : t::gFALSE;
       }
 
 
       public: static SVM_CLASS_METHOD(is_greater)
       {
-         SVM_ASSERT_INT(argv[0]);
-         SVM_ASSERT_INT(argv[1]);
+         t::Int::assert(argv[0]);
+         t::Int::assert(argv[1]);
 
-         svm::Object* result;
+         t::Object* result;
 
-         if (((svm::Int*)argv[0])->value > ((svm::Int*)argv[1])->value)
+         if (((t::Int*)argv[0])->value > ((t::Int*)argv[1])->value)
          {
-            result = (svm::Object*)svm::True;
+            result = (t::Object*)t::gTRUE;
          }
          else
          {
-            result = (svm::Object*)svm::False;
+            result = (t::Object*)t::gFALSE;
          }
          return result;
 
@@ -83,18 +75,18 @@ namespace modules { namespace system {
       public: static SVM_CLASS_METHOD(is_greater_or_equal)
       {
          ASSERT(argc == 2, "Bad argument count.");
-         SVM_ASSERT_INT(argv[0]);
-         SVM_ASSERT_INT(argv[1]);
+         t::Int::assert(argv[0]);
+         t::Int::assert(argv[1]);
 
-         svm::Object* result;
+         t::Object* result;
 
-         if (((svm::Int*)argv[0])->value >= ((svm::Int*)argv[1])->value)
+         if (((t::Int*)argv[0])->value >= ((t::Int*)argv[1])->value)
          {
-            result = (svm::Object*)svm::True;
+            result = (t::Object*)t::gTRUE;
          }
          else
          {
-            result = (svm::Object*)svm::False;
+            result = (t::Object*)t::gFALSE;
          }
          return result;
 
@@ -102,7 +94,7 @@ namespace modules { namespace system {
 
       public: static SVM_CLASS_METHOD(multiply)
       {
-         svm::Int::multiply_to(argv[0], argv[1]);
+         t::Int::multiply_to(argv[0], argv[1]);
 
          return argv[0];
       }
@@ -110,83 +102,83 @@ namespace modules { namespace system {
       public: static SVM_CLASS_METHOD(print)
       {
          ASSERT(argc == 1, "Bad argument count.");
-         SVM_ASSERT_INT(argv[0]);
+         t::Int::assert(argv[0]);
 
-         svm::Int::print(argv[0]);
+         t::Int::print(argv[0]);
 
-         return svm::Null;
+         return t::gNULL;
       }
 
       public: static SVM_CLASS_METHOD(subtract)
       {
          ASSERT(argc == 2, "-=(system.Int, system.Int). %d parameters given.", argc);
-         SVM_ASSERT_INT(argv[0]);
-         SVM_ASSERT_INT(argv[1]);
+         t::Int::assert(argv[0]);
+         t::Int::assert(argv[1]);
 
-         svm::Int::subtract_to(argv[0], argv[1]);
+         t::Int::subtract_to(argv[0], argv[1]);
 
          return argv[0];
       }
 
       public: static void declare_methods(svm::Engine& engine)
       {
-         SVM_ASSERT_NOT_NULL(svm::int_type);
+         SVM_ASSERT_NOT_NULL(t::tINT);
 
-         METHOD(svm::int_type, `*=', multiply)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::int_type)
+         METHOD(t::tINT, `*=', multiply)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tINT)
 
-         METHOD(svm::int_type, `+=', add)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::int_type)
+         METHOD(t::tINT, `+=', add)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tINT)
 
-         METHOD(svm::int_type, `compare_to', compare_to)
-         PARAM(`self', svm::int_type)
-         PARAM(`another_int', svm::int_type)
-         RETURNS(svm::int_type)
+         METHOD(t::tINT, `compare_to', compare_to)
+         PARAM(`self', t::tINT)
+         PARAM(`another_int', t::tINT)
+         RETURNS(t::tINT)
 
-         METHOD(svm::int_type, `equals', equals)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::bool_type)
+         METHOD(t::tINT, `equals', equals)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tBOOL)
 
-         METHOD(svm::int_type, `>=', is_greater_or_equal)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::bool_type)
+         METHOD(t::tINT, `>=', is_greater_or_equal)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tBOOL)
 
-         METHOD(svm::int_type, `>', is_greater)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::bool_type)
+         METHOD(t::tINT, `>', is_greater)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tBOOL)
 
-         METHOD(svm::int_type, `print', print)
-         PARAM(`self', svm::int_type)
-         RETURNS(svm::object_type)
+         METHOD(t::tINT, `print', print)
+         PARAM(`self', t::tINT)
+         RETURNS(t::tOBJECT)
 
-         METHOD(svm::int_type, `-=', subtract)
-         PARAM(`self', svm::int_type)
-         PARAM(`i', svm::int_type)
-         RETURNS(svm::int_type)
+         METHOD(t::tINT, `-=', subtract)
+         PARAM(`self', t::tINT)
+         PARAM(`i', t::tINT)
+         RETURNS(t::tINT)
       }
 
       public: static void finalize()
       {
-         ASSERT_NULL(svm::int_type);
+         ASSERT_NULL(t::tINT);
       }
 
       public: static void initialize(svm::Engine& engine)
       {
-         svm::int_type = engine.classes.declare_class("system.Int", svm::object_type);
-         SVM_ASSERT_CLASS(svm::int_type);
-         SVM_PICK(svm::int_type);
+         t::tINT = engine.classes.declare_class("system.Int", t::tOBJECT);
+         t::Class::assert(t::tINT);
+         t::Object::pick(t::tINT);
       }
 
       public: static void tear_down(svm::Engine& engine)
       {
-         SVM_DROP(svm::int_type);
+         t::Object::drop(t::tINT);
       }
    };
 
