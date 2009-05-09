b2b55841e033677840a25a4d29c9ca98e76ffca8
thomas <thomas.quemard@gmail.com>
2009-05-09 15:53:11 +0200

    main.cpp : + option --info

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ main (int argc, char **argv)
    {
       static struct option long_options[] =
       {
-         {"initialize", no_argument, 0, 'i'}
+         {"initialize", no_argument, 0, 's'},
+         {"info", no_argument, 0, 'i'}
       };
       int option_index = 0;
       c = getopt_long (argc, argv, "", long_options, &option_index);
@@ -24,10 +25,13 @@ main (int argc, char **argv)
 
       switch (c)
       {
-         case 'i':
+         case 's':
             //vm::EngineStarter* is = new vm::EngineStarter();
             //is->setup();
             //is->tear_down();
+            break;
+         case 'i':
+            vm::Engine::print_version();
             break;
 
          default:
