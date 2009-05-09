d4a33b158e8e130808565b7cc8039a78a5785989
thomas <thomas.quemard@gmail.com>
2009-05-09 15:20:53 +0200

    t/Exception : More doc

--- include/t/Exception.h
+++ include/t/Exception.h
@@ -27,8 +27,19 @@ namespace NS_TYPE
          return T_OBJECT::assert_type(obj, NS_TYPE::tEXCEPTION);
       }
 
+      /**
+       * Builds an empty exception.
+       */
       public: static T_OBJECT* build();
+
+      /**
+       * Builds an exception with message <message>.
+       */
       public: static T_OBJECT* build(T_OBJECT* message);
+
+      /**
+       * Builds a string from an exception <error>.
+       */
       public: static T_OBJECT* cast_to_string(T_OBJECT* error);
 
       /*
@@ -39,8 +50,19 @@ namespace NS_TYPE
          return T_OBJECT::check_type(obj, NS_TYPE::tEXCEPTION);
       }
 
+      /**
+       * Builds a string from exception <error> and print it to the console.
+       */
       public: static void print(T_OBJECT* error);
+
+      /**
+       * Sets the message of exception <error> to <message>.
+       */
       public: static T_OBJECT* set_message(T_OBJECT* error, T_OBJECT* message);
+
+      /**
+       * Sets the stack trace of exception <exception> to <stack_trace>.
+       */
       public: static T_OBJECT* set_stack_trace(T_OBJECT* exception, T_OBJECT* stack_trace);
    };
 }
