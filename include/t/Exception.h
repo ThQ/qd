bf723d168724722ee3ea59741d30c6e781b6dae6
thomas <thomas.quemard@gmail.com>
2009-05-23 15:07:50 +0200

    t/Exception : + doc

--- include/t/Exception.h
+++ include/t/Exception.h
@@ -12,15 +12,23 @@ namespace NS_TYPE
    extern T_OBJECT* tRUNTIME_EXCEPTION;
    extern T_OBJECT* tINDEX_OUT_OF_RANGE_EXCEPTION;
 
+   /**
+    * An exception signaling an error in a program flow.
+    */
    class Exception : public T_OBJECT
    {
       public: T_OBJECT* message;
       public: T_OBJECT* stack_trace;
 
+      /**
+       * Constructor.
+       */
       public: Exception();
 
       /**
-       * Asserts that an object is of type t::tEXCEPTION.
+       * Asserts that an object is of type t::Exception.
+       *
+       * @param obj Object to test
        */
       public: inline static void assert(T_OBJECT* obj)
       {
@@ -29,21 +37,32 @@ namespace NS_TYPE
 
       /**
        * Builds an empty exception.
+       *
+       * @return An emtpy t::Exception
        */
       public: static T_OBJECT* build();
 
       /**
        * Builds an exception with message <message>.
+       *
+       * @param message Error message
+       * @return A t::Exception with a message
        */
       public: static T_OBJECT* build(T_OBJECT* message);
 
       /**
        * Builds a string from an exception <error>.
+       *
+       * @param error The exception from which to build a string
+       * @return a t::String representing the exception
        */
       public: static T_OBJECT* cast_to_string(T_OBJECT* error);
 
       /*
-       * Checks if an object is of type t::tEXCEPTION.
+       * Checks if an object is of type t::Exception.
+       *
+       * @param obj An object to be checked.
+       * @return true if <obj> is of type t::Exception
        */
       public: inline static bool check(T_OBJECT* obj)
       {
@@ -51,17 +70,27 @@ namespace NS_TYPE
       }
 
       /**
-       * Builds a string from exception <error> and print it to the console.
+       * Builds a string from exception <error> and prints it to the console.
+       *
+       * @param error An exception that will be converted to string so as to be printed
        */
       public: static void print(T_OBJECT* error);
 
       /**
        * Sets the message of exception <error> to <message>.
+       *
+       * @param error The exception to set the message to
+       * @param message The message to use
+       * @return The message (unchanged)
        */
       public: static T_OBJECT* set_message(T_OBJECT* error, T_OBJECT* message);
 
       /**
        * Sets the stack trace of exception <exception> to <stack_trace>.
+       *
+       * @param exception The exception to set the stack trace to
+       * @param stack_trace The stack trace to use
+       * @return The stack trace (unchanged)
        */
       public: static T_OBJECT* set_stack_trace(T_OBJECT* exception, T_OBJECT* stack_trace);
    };
