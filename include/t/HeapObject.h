8de6ffb60e767e6fbf0d1b1c24e4b5503fd018fe
thomas <thomas.quemard@gmail.com>
2009-05-23 19:33:49 +0200

    t/HeapObject : + doc

--- include/t/HeapObject.h
+++ include/t/HeapObject.h
@@ -7,12 +7,29 @@ namespace NS_TYPE
 {
    extern T_OBJECT* tHEAP_OBJECT;
 
+   /**
+    * A heap object is an object representing an object from the heap.
+    *
+    * An heap object is replaced by the object pointed just before an opcode
+    * call.
+    */
    class HeapObject : public T_OBJECT
    {
       public: bool local;
       public: long position;
 
+      /**
+       * Constructor.
+       */
       public: HeapObject();
+
+      /**
+       * Creates a t::HeapObject.
+       *
+       * @param local Whether the object is from the local heap or not.
+       * @param position The position from the heap.
+       * @return A new t::HeapObject.
+       */
       public: static T_OBJECT* build(bool local, long position);
    };
 }
