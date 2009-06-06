46e63fe9cc6a8332d6dabf25cfd545fd14c2a986
thomas <thomas.quemard@gmail.com>
2009-06-06 14:38:09 +0200

    t/CoreFunction : +doc

--- include/t/CoreFunction.h
+++ include/t/CoreFunction.h
@@ -14,8 +14,13 @@ namespace NS_TYPE
       public: ULong function_pointer;
 
       /**
-       * Creates a t::CoreFunction object whose name is <name> and return type
-       * is <return_type>, it uses the function pointed by <pointer>.
+       * Creates a t::CoreFunction object whose name is [name] and return type
+       * is [return_type], it uses the function pointed by [pointer].
+       *
+       * @param name A pointer to a string containing the name of the function.
+       * @param pointer An integer that points to the function to call.
+       * @param return_type A pointer to a t::Class representing the return type.
+       * @return A pointer to the CoreFunction newly created.
        */
       public: static CoreFunction* build(const char* name, ULong pointer, T_OBJECT* return_type);
    };
