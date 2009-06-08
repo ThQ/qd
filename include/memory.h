94145f8539eff71ee4bb739236872334baf48271
thomas <thomas.quemard@gmail.com>
2009-06-08 13:40:28 +0200

    memory : + class Memory (free(), malloc(), realloc())

--- include/memory.h
+++ include/memory.h
@@ -5,4 +5,22 @@
 #define MALLOC(size) malloc((size))
 #define REALLOC(pointer, new_size) realloc((pointer), (new_size))
 
+class Memory
+{
+   public: inline static void* free(void* ptr)
+   {
+      return free(ptr);
+   }
+
+   public: inline static void* malloc(size_t size)
+   {
+      return malloc(size);
+   }
+
+   public: inline static void* realloc(void* ptr, size_t new_size)
+   {
+      return realloc(ptr, new_size);
+   }
+};
+
 #endif
