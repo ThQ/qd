f5c08d560ffa71716a4fad8dc1ca613e2d27b67a
thomas <thomas.quemard@gmail.com>
2009-06-13 16:12:18 +0200

    Memory.h : + doc

--- include/Memory.h
+++ include/Memory.h
@@ -10,16 +10,32 @@
  */
 class Memory
 {
+   /**
+    * Frees a memory allcated with malloc.
+    *
+    * @param ptr A pointer to the memory to free.
+    */
    public: inline static void* free(void* ptr)
    {
       return free(ptr);
    }
 
+   /**
+    * Allocates memory.
+    *
+    * @param size Size of the memory to allocate.
+    */
    public: inline static void* malloc(size_t size)
    {
       return malloc(size);
    }
 
+   /**
+    * Reallocates memory.
+    *
+    * @param ptr A pointer to the memory to reallocate.
+    * @param new_size The memory size to reallocate.
+    */
    public: inline static void* realloc(void* ptr, size_t new_size)
    {
       return realloc(ptr, new_size);
