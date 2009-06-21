9739ab56bca6e1cd31e63db74ee7fba2f2a1d2f3
thomas <thomas.quemard@gmail.com>
2009-06-21 22:37:11 +0200

    t/Array : - util func (the ones using OBJECT* as type for Array*), + doc, now using t::Object::type attr

--- include/t/Array.h
+++ include/t/Array.h
@@ -1,74 +1,78 @@
 #ifndef T_ARRAY
 #define T_ARRAY t::Array
 
+#include "t/Class.h"
 #include "t/Int.h"
 #include "t/Collection.h"
 
 namespace t
 {
-   extern Object* tARRAY;
+   //extern Object* tARRAY;
 
    /**
     * A @cls{t::Array} is a list of @cls{t::Object}'s who can only contain elements of a
     * predetermined type and who can never exceed a predetermined (and fixed) number of
     * elements.
+    * @todo Attribute [length] is from t::Collection, remove [item_count].
     */
    class Array: public Collection
    {
-      public: Object* type; ///< A pointer to a @cls{t::Class} representing the class of the objects stored.
-      public: Object** items; ///< The @cls{t::Object} stored.
-      /// @todo attribute [length] is from t::Collection, remove [item_count].
-      public: ULong item_count; ///< Number of item stored.
+      public: ushort item_type;     ///< What kind of object this items are (Bool, Array, String, UserObject).
+      public: Object* item_class;   ///< A pointer to a @cls{t::Class} representing the class of the objects stored, if needed, NULL otherwise.
+      public: Object** items;       ///< The @cls{t::Object} stored.
+      public: ULong item_count;     ///< Number of item stored. REPLACE THIS WITH Collection::length.
 
       /**
-       * Constructor.
+       * @brief Default constructor.
        */
-      public: Array();
+      public: Array ();
+
+      /**
+       * @brief Constructs an array whose type and length are known.
+       *
+       * @param nItemType A ushort describing the type of the items stored in the aray.
+       * @param pItemClass A pointer to a @cls{t::Class} to use as item type.
+       * @param dwArrayLength The length of the array to create.
+       */
+      public: Array (ushort nItemType, Class* pItemClass, UInt64 dwArrayLength);
+
+      /**
+       * @brief Constructs an array whose type and length are known.
+       *
+       * @param nItemType A ushort describing the type of the items stored in the aray.
+       * @param pItemClass A pointer to a @cls{t::Class} to use as item type.
+       * @param dwArrayLength The length of the array to create.
+       * @param pItems An array of @cls{t::Object}'s to fill the array. It must
+       * contain at least @prm{dwArrayLength} objects.
+       */
+      public: Array (ushort nItemType, Class* pItemClass, UInt64 dwArrayLength, Object** pItems);
 
       /**
        * Destructor.
        */
-      public: ~Array();
+      public: ~Array ();
 
       /**
-       * Asserts that an object is of type @cls{t::Array}
-       *
-       * @param obj The object to check.
+       * Initializes an Array object.
        */
-      public: inline static void assert(T_OBJECT* obj)
+      protected: inline void _init ()
       {
-         T_OBJECT::assert_type(obj, NS_TYPE::tARRAY);
+         this->type = t::ARRAY_TYPE;
+         this->item_type = t::UNDEFINED_TYPE;
+         this->item_class = NULL;
+         this->items = NULL;
+         this->item_count = 0;
       }
 
       /**
-       * Creates a t::Array object of type @prm{array_type} and length @prm{array_length}.
-       * @param array_type A pointer to a @cls{t::Class} to use a array type.
-       * @param array_length A pointer to a @cls{t::Int} representing the length of the array to create.
-       * @return A pointer to a @cls{t::Array} newly created.
-       */
-      public: static Object* build(T_OBJECT* array_type, T_OBJECT* array_length);
-
-      /**
-       * Creates a @cls{t::Array} object of type @prm{array_type} and length @prm{array_length}.
+       * Asserts that an object is of type t::ARRAY_TYPE.
        *
-       * @param array_type A pointer to a @cls{t::Class} to use a array type.
-       * @param array_length The length of the array to create.
-       * @return A pointer to a @cls{t::Array} newly created.
+       * @param obj The object to check.
        */
-      public: static Object* build(T_OBJECT* array_type, ULong array_length);
-
-      /**
-       * Creates a @cls{t::Array} object of type @prm{array_type} and length
-       * @prm{array_length} and who contains @prm{array_items}.
-       *
-       * Take care: @prm{array_length} must be the length of objects in @prm{items}.
-       *
-       * @param array_type A pointer to a @cls{t::Class} to use a array type.
-       * @param array_length The length of the array to create.
-       * @param array_items An array of @cls{t::Object}'s to populate the new array.
-       * @return A pointer to a @cls{t::Array} newly created.
-       */
-      public: static Object* build(T_OBJECT* array_type, ULong array_length, T_OBJECT** array_items);
+      public: inline static void assert(Object* obj)
+      {
+         obj->assert_type(t::ARRAY_TYPE);
+      }
 
       /**
        * Checks if an object is of type @cls{t::Array}.
@@ -78,7 +82,7 @@ namespace t
        */
       public: inline static bool check(T_OBJECT* obj)
       {
-         return T_OBJECT::check_type(obj, NS_TYPE::tARRAY);
+         return T_OBJECT::check_type(obj, t::ARRAY_TYPE);
       }
 
       /**
@@ -87,34 +91,11 @@ namespace t
       public: void clear();
 
       /**
-       * Replaces each item of a @cls{t::Array} with t::gNULL.
-       *
-       * @param arr A pointer to a @cls{t::Array} to clear.
-       */
-      public: inline static void clear (T_OBJECT* arr)
-      {
-         Array::assert(arr);
-         ((Array*)arr)->clear();
-      }
-
-      /**
        * Replaces an item of a @cls{t::Array} with t::gNULL.
        *
        * @param item_index The index of the item to replace with t::gNULL.
        */
-      public: void clear_item (ULong item_index);
-
-      /**
-       * Replaces an item of a @cls{t::Array} with t::gNULL.
-       *
-       * @param array A pointer to a @cls{t::Array} to clear.
-       * @param item_index The index of the item to replace with t::gNULL.
-       */
-      public: inline static void clear_item (T_OBJECT* array, ULong item_index)
-      {
-         Array::assert(array);
-         ((Array*)array)->clear_item(item_index);
-      }
+      public: void clear_item (UInt64 item_index);
 
       /**
        * Replaces a range of items with t::gNULL.
@@ -122,20 +103,7 @@ namespace t
        * @param range_start The index at which to start clearing.
        * @param range_end The index at which to stop clearing.
        */
-      public: void clear_range(ULong range_start, ULong range_end);
-
-      /**
-       * Replaces a range of items with t::gNULL.
-       *
-       * @param array A pointer to a @cls{t::Array} to clear.
-       * @param start The index at which to start clearing.
-       * @param end The index at which to stop clearing.
-       */
-      public: inline static void clear_range(T_OBJECT* array, ULong start, ULong end)
-      {
-         Array::assert(array);
-         ((Array*)array)->clear_range(start, end);
-      }
+      public: void clear_range(UInt64 range_start, UInt64 range_end);
 
       /**
        * Replaces an item with another one.
@@ -146,40 +114,12 @@ namespace t
       public: void set_item(ULong item_index, T_OBJECT* new_item);
 
       /**
-       * Replaces an item with another one.
-       *
-       * @param array A pointer to a @cls{t::Array} to set an item to.
-       * @param item_index The index of the item to set.
-       * @param new_item A pointer to an @cls{t::Object} to use as a replacement.
-       */
-      public: static void set_item(T_OBJECT* array, ULong item_index, Object* new_item)
-      {
-         Array::assert(array);
-         T_OBJECT::assert_not_null(new_item);
-         ((Array*)array)->set_item(item_index, new_item);
-      }
-
-      /**
        * Sets the type of the objects that can possibly be stored in a
        * @cls{t::Array}.
        *
        * @param type A pointer to a @cls{t::Class} to use a type.
        */
-      public: void set_type(T_CLASS* type);
-
-      /**
-       * Sets the type of the objects that can possibly be stored in a
-       * @cls{t::Array}.
-       *
-       * @param array A pointer to a @cls{t::Array} to set the type.
-       * @param type A pointer to a @cls{t::Class} to use a type.
-       */
-      public: inline static void set_type(T_OBJECT* array, T_OBJECT* type)
-      {
-         Array::assert(array);
-         T_CLASS::assert(type);
-         ((Array*)array)->set_type((T_CLASS*)type);
-      }
+      public: void set_type(ushort type, Class* cls);
 
       /**
        * Allocates memory for @prm{array_size} items in array [this->items] and
@@ -187,7 +127,7 @@ namespace t
        *
        * @param array_size The size of the array.
        */
-      public: void size (ULong array_size);
+      public: void size (UInt64 array_size);
    };
 }
 
--- src/t/Array.cpp
+++ src/t/Array.cpp
@@ -4,133 +4,131 @@ namespace t
 {
    Array::Array ()
    {
-      this->type = NULL;
-      this->items = NULL;
-      this->item_count = 0;
-      this->set_class(NS_TYPE::tARRAY);
+      this->_init();
+   }
+
+   Array::Array (ushort nItemType, Class* pItemClass, UInt64 length)
+   {
+      this->_init();
+      this->set_type(nItemType, pItemClass);
+      this->size(length);
+   }
+
+   Array::Array (ushort nItemType, Class* pItemClass, UInt64 dwArrayLength, Object** pItems)
+   {
+      this->_init();
+      this->set_type(nItemType, pItemClass);
+      this->size(dwArrayLength);
+
+      LOOP_FROM_TO(UInt64, i, 0, dwArrayLength)
+      {
+         ASSERT_NULL(this->items[i]);
+         ASSERT_NOT_NULL(pItems[i]);
+         pItems[i]->assert_type((ushort)nItemType);
+
+         pItems[i]->pick();
+         this->items[i] = pItems[i];
+      }
    }
 
    Array::~Array ()
    {
-      T_OBJECT::assert_not_null(this->type);
-
-      T_OBJECT::drop(this->type);
-      for (ULong item_index = 0 ; item_index < this->item_count ; ++item_index)
+      LOOP_FROM_TO(UInt64, i, 0, this->item_count)
       {
-         T_OBJECT::assert_not_null(this->items[item_index]);
-         T_OBJECT::drop(this->items[item_index]);
-         this->items[item_index] = NULL;
-      }
-   }
-
-   T_OBJECT*
-   Array::build (T_OBJECT* type, T_OBJECT* len)
-   {
-      return Array::build(type, ((T_INT*)len)->value);
-   }
-
-   T_OBJECT*
-   Array::build (T_OBJECT* type, ULong array_length)
-   {
-      T_OBJECT::assert_not_null(type);
-
-      Array* result = new Array();
-      Array::set_type(result, type);
-      result->size(array_length);
-
-      return (T_OBJECT*)result;
-   }
-
-   T_OBJECT*
-   Array::build (T_OBJECT* type, ULong len, T_OBJECT** items)
-   {
-      T_OBJECT::assert_not_null(type);
-      // ASSERT(len != 0, "Length should not be 0.");
-
-      Array* result = new Array();
-      Array::set_type(result, type);
-      result->item_count = len;
-      result->items = items;
-
-      for (ULong item_index = 0 ; item_index < len ; ++ item_index)
-      {
-         T_OBJECT::pick(items[item_index]);
+         ASSERT_NOT_NULL(this->items[i]);
+         this->items[i]->drop();
+         this->items[i] = NULL;
       }
 
-      return (T_OBJECT*)result;
+      if (this->item_class != NULL)
+      {
+         this->item_class->drop();
+      }
    }
 
    void
    Array::clear ()
    {
-      for (ULong item_index = 0 ; item_index < this->item_count ; ++item_index)
+      LOOP_FROM_TO(UInt64, item_index, 0, this->item_count)
       {
-         T_OBJECT::drop_safe(this->items[item_index]);
-         T_OBJECT::pick(NS_TYPE::gNULL);
-         this->items[item_index] = NS_TYPE::gNULL;
+         ASSERT_NOT_NULL(this->items[item_index]);
+
+         this->items[item_index]->drop();
+         this->items[item_index] = t::gNULL;
+         t::gNULL->pick();
       }
    }
 
    void
-   Array::clear_item (ULong item_index)
+   Array::clear_item (UInt64 item_index)
    {
-      CHECK(item_index >= this->item_count, "Index out of range.");
+      ASSERT(item_index >= this->item_count, "Index out of range.");
+      ASSERT_NOT_NULL(this->items[item_index]);
 
-      T_OBJECT::drop_safe(this->items[item_index]);
-      T_OBJECT::pick(NS_TYPE::gNULL);
-      this->items[item_index] = NS_TYPE::gNULL;
+      this->items[item_index]->drop();
+      this->items[item_index] = t::gNULL;
+      t::gNULL->pick();
    }
 
    void
-   Array::clear_range (ULong range_start, ULong range_end)
+   Array::clear_range (UInt64 range_start, UInt64 range_end)
    {
       ASSERT(range_start < range_end, "Start index must be lower than end index.");
       ASSERT(range_end < this->item_count - 1, "End index must be lower than the item count.");
 
-      for (ULong item_index = range_start ; item_index < range_end ; item_index ++)
+      LOOP_FROM_TO(UInt64, item_index, range_start, range_end)
       {
-         T_OBJECT::assert_not_null(this->items[item_index]);
-         T_OBJECT::drop(this->items[item_index]);
-         T_OBJECT::pick(t::gNULL);
+         ASSERT_NOT_NULL(this->items[item_index]);
+
+         this->items[item_index]->drop();
          this->items[item_index] = t::gNULL;
+         t::gNULL->pick();
       }
    }
 
    void
    Array::set_item (ULong item_index, T_OBJECT* new_item)
    {
-      T_OBJECT::assert_not_null(new_item);
-      T_OBJECT::drop(this->items[item_index]);
-      T_OBJECT::pick(new_item);
+      ASSERT_NOT_NULL(new_item);
+      ASSERT_NOT_NULL(this->items[item_index]);
+
+      this->items[item_index]->drop();
+
+      new_item->pick();
       this->items[item_index] = new_item;
    }
 
    void
-   Array::set_type (T_CLASS* type)
+   Array::set_type (ushort type, Class* cls)
    {
-      t::Class::assert(type);
-      T_OBJECT::drop_safe(this->type);
-      T_OBJECT::pick(type);
-      this->type = type;
-   }
+      ASSERT_NULL(this->cls);
 
-   void
-   Array::size (ULong array_size)
-   {
-      // A 0-length array may be useful : A function may want to return an empty array.
-      // ASSERT(array_size != 0, "An Array of 0-length must not be created.");
-
-      this->item_count = array_size;
-      this->items = (T_OBJECT**)MALLOC(sizeof(T_OBJECT*) * array_size);
-
-      for (ULong item_index = 0 ; item_index < array_size ; item_index ++)
+      this->item_type = type;
+      this->item_class = cls;
+      if (cls != NULL)
       {
-         this->items[item_index] = t::gNULL;
-         T_OBJECT::pick(t::gNULL);
+         cls->pick();
       }
    }
 
-   T_OBJECT* tARRAY = NULL;
+   void
+   Array::size (UInt64 array_size)
+   {
+      // A 0-length array may be useful : A function may want to return an empty array.
+      // ASSERT(array_size != 0, "An Array of 0-length must not be created.");
+      ASSERT(this->item_count == 0, "Cannot size an array already sized.");
+      ASSERT(this->items == NULL, "Cannot size an array that is already containing objects.");
+      this->item_count = array_size;
+      this->items = (T_OBJECT**) Memory::malloc(sizeof(T_OBJECT*) * array_size);
+
+      LOOP_FROM_TO(UInt64, i, 0, array_size)
+      {
+         this->items[i] = t::gNULL;
+         t::gNULL->pick();
+      }
+   }
+
+   //T_OBJECT* tARRAY = NULL;
 }
 
 
