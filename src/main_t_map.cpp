8ebeb4ae7363c33e963cd94953fae2f3e7f1e965
thomas <thomas.quemard@gmail.com>
2009-06-22 19:20:32 +0200

    main_t_map.cpp : using util::Object::drop|pick instead of t::Object::drop|pick

--- src/main_t_map.cpp
+++ src/main_t_map.cpp
@@ -1,7 +1,25 @@
 #include "t/Map.h"
+#include "Stats.h"
 
 int
 main (int argc, char** argv)
 {
+   t::Map* pBaseMap = new t::Map();
+   util::Object::pick(pBaseMap);
+
+   t::Map* pKey = new t::Map();
+   util::Object::pick(pKey);
+
+   t::Map* pValue = new t::Map();
+   util::Object::pick(pValue);
+
+   pBaseMap->set_item(pKey, pValue);
+
+   util::Object::drop(pValue);
+   util::Object::drop(pKey);
+   util::Object::drop(pBaseMap);
+
+   Stats.print_report();
+
    return 0;
 }
