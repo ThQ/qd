a7f8fdd2cab85b70deeb07ca59dd4e609b1b25b0
thomas <thomas.quemard@gmail.com>
2009-06-25 11:34:59 +0200

    src/main_t_string.cpp : + some basic string manipulation

--- src/main_t_string.cpp
+++ src/main_t_string.cpp
@@ -3,6 +3,24 @@
 int
 main (int argc, char** argv)
 {
+   t::String* pBaseString = new t::String("FUCK ME");
+   pBaseString->pick();
+
+   t::String* pAString = new t::String("_-+");
+   pAString->pick();
+
+   t::String* pCenteredString = pBaseString->center((t::UInt64)20, pAString);
+   pCenteredString->pick();
+   pCenteredString->fpPrintLine(pCenteredString);
+
+   t::String* pLowerCasedString = pBaseString->lower_case();
+   pLowerCasedString->pick();
+   pLowerCasedString->fpPrintLine(pLowerCasedString);
+
+   pLowerCasedString->drop();
+   pCenteredString->drop();
+   pAString->drop();
+   pBaseString->drop();
 
    Stats.print_report();
 
