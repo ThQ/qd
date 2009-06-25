b7593879c2f9d8c80b16c6cb95d8bfea124f6950
thomas <thomas.quemard@gmail.com>
2009-06-25 21:19:22 +0200

    util/Object: rem all methods (already in t::Object)

--- include/util/Object.h
+++ include/util/Object.h
@@ -1,8 +1,8 @@
 #ifndef UTIL_OBJECT
 #define UTIL_OBJECT NS_UTIL::Object
 
-#include "t/Class.h"
 #include "t/Object.h"
+#include "vm/Class.h"
 #include "Stats.h"
 
 namespace util
@@ -12,29 +12,6 @@ namespace util
     */
    class Object
    {
-      /**
-       * Asserts that an object is of a certain type.
-       *
-       * @param object The object to check.
-       * @param type The supposed type of @prm{object}.
-       */
-      public: static void assert_type(t::Object* object, t::Object* type);
-
-      /**
-       * Decreases the reference count of an object by one.
-       *
-       * @param pObject The object to drop.
-       * @return true if everything went well.
-       */
-      public: static bool drop(t::Object* pObject);
-
-      /**
-       * Increases the reference count of an object by one.
-       *
-       * @param pObject The object to pick.
-       * @return true if everything went well.
-       */
-      public: static bool pick(t::Object* pObject);
    };
 }
 
