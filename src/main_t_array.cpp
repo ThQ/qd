abd4df59071232c927cf14987d52aea4648c7415
thomas <thomas.quemard@gmail.com>
2009-07-10 22:32:54 +0200

    better logging
    + basic operations on literal arrays

--- src/main_t_array.cpp
+++ src/main_t_array.cpp
@@ -1,42 +1,28 @@
 #include "t/Array.h"
+#include "Stats.h"
 
 int
 main (int argc, char** argv)
 {
-   t::Array* pArray = new t::Array(t::ARRAY_TYPE, NULL, 1);
-   pArray->pick();
+   t::Array* pArrayArray = new t::Array(t::LIST_TYPE, &t::cARRAY, 1);
+   pArrayArray->pick();
 
-   t::Array* pAnotherArray = new t::Array;
+   t::Array* pAnotherArray = new t::Array(t::LIST_TYPE, &t::cARRAY, 0);
    pAnotherArray->pick();
-
-   INTERNAL(
-         "<%s @%x> SET_ITEM (.at 0, .item <%s @%x>)\n",
-         t::cast_type_to_string(pArray->type),
-         (uint)pArray,
-         t::cast_type_to_string(pAnotherArray->type),
-         (uint)pAnotherArray
-   );
-   pArray->set_item(0, pAnotherArray);
-
-   INTERNAL(
-         "<%s @%x> CLEAR_ITEM (.at 0)\n",
-         t::cast_type_to_string(pArray->type),
-         (uint)pArray
-   );
-   pArray->clear_item(0);
-
-   INTERNAL(
-         "<%s @%x> SET_ITEM (.at 0, .item <%s @%x>)\n",
-         t::cast_type_to_string(pArray->type),
-         (uint)pArray,
-         t::cast_type_to_string(pAnotherArray->type),
-         (uint)pAnotherArray
-   );
-   pArray->set_item(0, pAnotherArray);
-   pArray->clear_range(0, 1);
-
+   pArrayArray->set_item(0, pAnotherArray);
+   pArrayArray->clear_item(0);
+   pArrayArray->set_item(0, pAnotherArray);
+   pArrayArray->clear_item(0);
    pAnotherArray->drop();
+   pArrayArray->drop();
 
-   pArray->drop();
+
+   t::Array* pInt16Array = new t::Array(t::INT16_TYPE, NULL, 1);
+   pInt16Array->pick();
+   pInt16Array->set_item(0, (t::Value)16);
+   pInt16Array->clear_item(0);
+   pInt16Array->drop();
+
+   Stats.print_report();
    return 0;
 }
